b2265822efeeaa138c8f58b3862863c5b6497af7
Anil Gurjar <115614579+gurjaranil13@users.noreply.github.com>
2023-12-18 09:43:40 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345
+let accountEamil = "anil@google.com"
+var accountPass = "12345"
+accountCity = "Rajasthan"
+let accountState;
+//accountId = 2
+
+console.log(accountId);
+console.table([accountId, accountEamil, accountPass, accountCity, accountState]);
