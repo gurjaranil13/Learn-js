dd064f2fd9b53e675eff67042ad9e0b1c62214fc
Anil Gurjar <115614579+gurjaranil13@users.noreply.github.com>
2023-12-20 10:16:51 +0000

    played with strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,24 @@
+const name = "Anil"
+const repoCount = 2
+
+// console.log(`Hello my name is ${name} and repo count is ${repoCount}`);
+
+const gameName = new String("Counter Strike")
+// console.log(gameName.substring(0,4))
+// console.log(gameName.slice(-12,4))
+
+const newString = "   Anil gurjar   "
+// console.log(newString.trim()) //Removes front and back spaces from the string
+// console.log(newString)
+
+const url = "https://github.com/%20gurjaranil13/%20Learn-js"
+
+// console.log(url.replaceAll('%20', '_')); //replace is used to remove only one thing and replaceAll is used to replace all things.
+// console.log(url)
+
+// console.log(url.includes('gut')) //Checks gut is present in string or not
+
+// console.log(gameName.split('S'))
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('u'));
