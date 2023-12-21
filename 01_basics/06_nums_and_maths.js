f3e1ccb1d1751df92e2d74c2b8d56da45ee0b4fd
Anil Gurjar <115614579+gurjaranil13@users.noreply.github.com>
2023-12-21 12:22:42 +0000

    learn about number and Math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,33 @@
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+
+// console.log(balance.toFixed(1))
+
+const otherNumber = 2.474
+// console.log(otherNumber.toPrecision(2))
+
+const hundred = 10000000
+// console.log(hundred.toLocaleString('en-IN'))
+
+// +++++++++++++++++++++Maths+++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4.5)) //get absolute value
+// console.log(Math.round(otherNumber)) 
+
+// console.log(Math.ceil(4.6)) //give 5 
+
+// console.log(Math.floor(4.9)) //give 4
+
+// console.log(Math.min(4,6,7,8,9))
+// console.log(Math.max(4,6,7,8,9))
+
+// console.log(Math.floor(Math.random() * 10 + 1))
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * max-min+1) + min)
+
+
