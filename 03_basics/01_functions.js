ed0b3e1d1e3747cb8258f89c7a2fae252bda38a9
Anil Gurjar <115614579+gurjaranil13@users.noreply.github.com>
2024-01-02 11:27:09 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,12 @@
+function myDetail(){
+    console.log("hello")
+}
+
+// myDetail()
+
+function addTwoNumbers(num1, num2){ // parameters
+    console.log(num1 + num2)
+}
+
+addTwoNumbers(1, 2) //arguments
+
