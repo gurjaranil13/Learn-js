e574f94172ce04da8536798a7d638f71c1426fe3
Anil Gurjar <115614579+gurjaranil13@users.noreply.github.com>
2024-01-18 10:54:39 +0530

    Update 01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,5 +8,9 @@ function addTwoNumbers(num1, num2){ // parameters
     console.log(num1 + num2)
 }
 
-addTwoNumbers(1, 2) //arguments
+// addTwoNumbers(1, 2) //arguments
 
+function myCart(num){
+    return num
+}
+console.log(myCart\(200))
