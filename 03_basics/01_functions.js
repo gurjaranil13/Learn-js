fe5fbc468d6bdc3bc4360e23f0dbf8b1b0684748
Anil Gurjar <115614579+gurjaranil13@users.noreply.github.com>
2024-01-18 06:27:57 +0000

    object handle and array handle through function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -10,7 +10,32 @@ function addTwoNumbers(num1, num2){ // parameters
 
 // addTwoNumbers(1, 2) //arguments
 
-function myCart(num){
+function myCart(...num){ //spread operator or rest operator - use to store mult values in function
     return num
 }
-console.log(myCart\(200))
+// console.log(myCart(200, 400, 300))
+
+const skybags = {
+    brand: 'skybag',
+    quantity: 1
+}
+
+function handleObject(anyobject){
+    console.log(`name is ${anyobject.brand} and quantity is ${anyobject.quantity}`);
+}
+
+// handleObject(skybags) //created a object and pass in this function
+// handleObject({
+//     brand: 'cello', //we can pass direct object here
+//     quantity: 2
+// })
+
+const cartPrices = [200,414,588,477]
+
+function getsecondPrice(getarray){
+    return getarray[1]
+}
+
+// console.log(getsecondPrice(cartPrices))
+// console.log(getsecondPrice([200,444,555,888]))
+
